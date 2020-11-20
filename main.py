02aeeb6b811b7d9fdd6f7517524123a5d0200f1b
João Miranda <dot.jmiranda@gmail.com>
2020-11-20 22:48:49 +0000

    added downloading to a different folder

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for a in soup.find_all("a"): # iterate through all <a> tags
     try:
         href = a["href"]
         if URL.split("/")[-1] in href: # check for duplicate links
-            if href not in music_urls:
+            if href not in music_urls and ".jpg" not in href:
                 music_urls.append(href)
     except KeyError:
         pass
@@ -26,18 +26,21 @@ for a in soup.find_all("a"): # iterate through all <a> tags
 def getDownloadURL(link):
     link_page = requests.get(BASE_URL + link).text
     link_soup = BeautifulSoup(link_page, "html.parser")
+    betterFlag = False
 
     for a in link_soup.find_all("a"):
         try:
             href = a["href"]
-            if "vgmsite.com" in href and ".flac" in href or ".m4a" in href:
-                if href not in download_urls and "jpg" not in href:
-                    download_urls.append(href)
-                    print(href)
-            elif "vgmsite.com" in href and ".mp3" in href:
-                if href not in download_urls and "jpg" not in href:
-                    download_urls.append(href)
-                    print(href)
+            if "vgmsite.com" in href:
+                if  ".flac" in href or ".m4a" in href:
+                    betterFlag = True
+                    if href not in download_urls and "jpg" not in href:
+                        download_urls.append(href)
+                        #print(href)
+                elif ".mp3" in href and betterFlag is True:
+                    if href not in download_urls and "jpg" not in href:
+                        download_urls.append(href)
+                        #print(href)
         except KeyError:
             pass
 
@@ -51,27 +54,31 @@ for thread in threads: # starts all threads
 for thread in threads: # waits for all threads to finish
     thread.join()
 
-with requests.Session() as req:
-    for file in download_urls:
-        print("Downloading File")
-        fileName = file.split("/")[-1]
-        fileRequest = req.get(file, stream=True)
-        with open(fileName, "wb") as musicFile:
-            musicFile.write(fileRequest.content)
-        print("Finished download File")
+print("Found", len(download_urls), "tracks.")
 
-for file in os.listdir():
-    if file.endswith(".m4a") or file.endswith(".flac") or file.endswith(".mp3"):
-        tag = TinyTag.get(file)
+if os.path.isdir("download"):
+    with requests.Session() as req:
+        save_path = "download/"
+        for file in download_urls:
+            print("Downloading file")
+            fileName = file.split("/")[-1]
+            fileRequest = req.get(file, stream=True)
+            with open(os.path.join(save_path, fileName), "wb") as musicFile:
+                musicFile.write(fileRequest.content)
+                print("Finished downloading file: " + musicFile.name)
+
+    for file in os.listdir("download"):
+        tag = TinyTag.get(os.path.join("download", file))
         newName = tag.title
+        filePath = os.path.join("download", file)
         if "/" in newName:
             newName = newName.replace("/", "-")
+        newNamePath = os.path.join("download", newName)
         if file.endswith(".m4a"):
-            tag = TinyTag.get(file)
-            os.rename(file, newName + ".m4a")
+            os.rename(filePath, newNamePath + ".m4a")
         elif file.endswith(".flac"):
-            tag = TinyTag.get(file)
-            os.rename(file, newName + ".flac")
+            os.rename(filePath, newNamePath + ".flac")
         elif file.endswith(".mp3"):
-            tag = TinyTag.get(file)
-            os.rename(file, newName + ".mp3")
+            os.rename(filePath, newNamePath + ".mp3")
+else:
+    print("Please create a folder named 'download' in order to download.")
